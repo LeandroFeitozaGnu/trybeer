edf15d80f5f94984f7f044b742a33910dbc1d504
Leandro <leandro.feitoza8018@gmail.com>
2021-03-23 21:51:10 -0300

    dicionario de mensagens de e status HTTP

---
+++ back-end/src/utils/userConstants.js
@@ -0,0 +1,27 @@
+module.exports = {
+  BAD_REQUEST: {
+    code: 400,
+    message: 'Invalid Entries. Try again.',
+    error: true,
+  },
+  CONFLICT: {
+    code: 409,
+    message: 'Email already registered',
+    error: true,
+  },
+  USER_NOT_FOUND: {
+    code: 401,
+    message: 'Usuário não cadastrado.',
+    error: true,
+  },
+  SQL_ERROR: {
+    code: 500,
+    message: 'Erro interno',
+    error: true,
+  },
+  USER_LOGIN: {
+    code: 200,
+    message: 'Login efetuado com sucesso',
+    error: false,
+  },
+};
